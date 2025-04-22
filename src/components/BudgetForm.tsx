a69c7641588cd3bf59c1067196497420a63810a9
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-04-22 21:09:59 +0200

    Formulario e inputs integrados

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,7 +1,40 @@
+import { useState }  from "react";
+
 
 
 export default function BudgetForm() {
+  
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber);
+  }
+
   return (
-    <div>BudgetForm</div>
-  )
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-2">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Definir Presupuesto
+        </label>
+        <input
+          id="budgetID"
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Añade tu presupuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+        ></input>
+      </div>
+
+      <input
+        type="submit"
+        value="Añadir presupuesto"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase "
+      />
+    </form>
+  );
 }
