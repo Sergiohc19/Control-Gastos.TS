78dc8e30bf98a3eedb81fe2f8cfae886ac4ddec7
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-05-11 22:47:22 +0200

    mostrando alerta si faltan campos que rellenar

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -4,6 +4,7 @@ import { categories } from "../data/categories";
 import DatePicker from "react-date-picker";
 import "react-calendar/dist/Calendar.css";
 import "react-date-picker/dist/DatePicker.css";
+import { ErrorMessage } from "./ErrorMessage";
 
 export default function ExpenseForm() {
   const [expense, setExpense] = useState<DraftExpense>({
@@ -13,26 +14,40 @@ export default function ExpenseForm() {
     date: new Date(),
   });
 
+  const [error, setError] = useState("");
+
   const handleChange = (
     e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
   ) => {
     const { name, value } = e.target;
     const isAmountField = ["amount"].includes(name);
-  setExpense({
-    ...expense, 
-    [name]: isAmountField ? +value : value,
-  });
+    setExpense({
+      ...expense,
+      [name]: isAmountField ? +value : value,
+    });
   };
 
   const handleChangeDate = (value: Value) => {
     setExpense({ ...expense, date: value });
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (Object.values(expense).includes("")) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+  };
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
         Nuevo Gasto
       </legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+
+
       <div className="flex flex-col gap-2">
         <label htmlFor="expenseName" className="text-xl">
           Nombre Gasto:
@@ -67,8 +82,11 @@ export default function ExpenseForm() {
         <label htmlFor="categoy" className="text-xl">
           Categoría:
         </label>
-        <select id="category" className="bg-slate-100 p-2" name="category"
-        onChange={handleChange}
+        <select
+          id="category"
+          className="bg-slate-100 p-2"
+          name="category"
+          onChange={handleChange}
         >
           <option value={expense.category}>-- Seleccione --</option>
           {categories.map((category) => (
