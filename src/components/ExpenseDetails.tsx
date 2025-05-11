c5e1593944815d276e56692c75b6471b0e134c6f
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-05-11 23:36:40 +0200

    Creando el Componente para mostrar los gastos

---
+++ src/components/ExpenseDetails.tsx
@@ -0,0 +1,12 @@
+import { Expense } from "../types";
+
+type ExpenseDetailsProps = {
+  expense: Expense
+ 
+  };
+
+export const ExpenseDetails = ({ expense } : ExpenseDetailsProps) => {
+  return (
+    <div>ExpenseDetails</div>
+  )
+}
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
+import { ExpenseDetails } from "./ExpenseDetails";
+
+export const ExpenseList = () => {
+  const { state } = useBudget();
+  const isEmpty = useMemo(
+    () => state.expense.length === 0,
+    [state.expense.length]
+  );
+
+  return (
+    <div className="mt-10 ">
+      {isEmpty ? (
+        <p className="text-gray-600 text-2xl font-bold">No hay gastos</p>
+      ) : (
+        <>
+          <p className="text-gray-600 text-2xl font-bold my-5">
+            Lista de Gastos.
+          </p>
+          {state.expense.map( expense => (
+
+          <ExpenseDetails 
+          key={expense.id}
+          expense={expense}
+          
+          />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
